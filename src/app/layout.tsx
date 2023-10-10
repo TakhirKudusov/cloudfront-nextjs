3c83beb2d972da552089139ccea1b65926b25472
Takhir Kudusov <kudusov.takhir@gmail.com>
2023-10-10 06:43:25 +0500

    [main]: antd registry removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,10 @@ import type { Metadata } from "next";
 import StyledComponentsRegistry from "@/lib/registry";
 import GlobalStyles from "@/styles/global";
 import Header from "@/UI kit/header";
-import AntdRegistry from "@/lib/AntdRegistry";
 import PageWrapper from "@/UI kit/pageWrapper";
 import ParticlesBg from "@/UI kit/particlesBg";
 import { Flip, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Footer from "@/UI kit/footer";
 
 export const metadata: Metadata = {
   title: "Cloudfront",
@@ -23,22 +21,20 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <StyledComponentsRegistry>
-          <AntdRegistry>
-            <GlobalStyles />
-            <Header />
-            <PageWrapper>{children}</PageWrapper>
-            <ToastContainer
-              transition={Flip}
-              autoClose={5_000}
-              position="bottom-right"
-              hideProgressBar={false}
-              closeOnClick={true}
-              pauseOnHover={true}
-              draggable={true}
-              theme="dark"
-            />
-            <ParticlesBg />
-          </AntdRegistry>
+          <GlobalStyles />
+          <Header />
+          <PageWrapper>{children}</PageWrapper>
+          <ToastContainer
+            transition={Flip}
+            autoClose={5_000}
+            position="bottom-right"
+            hideProgressBar={false}
+            closeOnClick={true}
+            pauseOnHover={true}
+            draggable={true}
+            theme="dark"
+          />
+          <ParticlesBg />
         </StyledComponentsRegistry>
       </body>
     </html>
